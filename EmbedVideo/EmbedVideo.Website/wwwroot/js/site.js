7fc35ce77dc9af1d95c9eb210684965010281d5e
EChristian74 <75714720+EChristian74@users.noreply.github.com>
2021-10-09 23:37:10 -0500

    VISUAL STUDIO HTML SUBMISSION ASSIGNMENT:
    In Visual Studio, create a website that includes the following:
    
    - A basic HTML webpage with an embedded music video from Youtube of one of your favorite songs,
    
    - Customize some element of the page with CSS (such as font or background color),
    
    - Using JavaScript, create some form of animation on the webpage.

---
+++ EmbedVideo/EmbedVideo.Website/wwwroot/js/site.js
@@ -0,0 +1,8 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Simple alert regarding upcoming festival for 2021
+
+function myFunction() {
+    alert("Join us for our 2021 festival on November 30!");
+}
